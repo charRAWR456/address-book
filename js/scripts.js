de0087e277db6e7ce56723f5220940f0d18af92f
Dillon Titcomb and Jamie Pittak <jamie@>
2018-01-10 13:40:18 -0800

    add multiple contacts

--- js/scripts.js
+++ js/scripts.js
@@ -4,17 +4,16 @@ $(document).ready(function() {
     var address = $("input#address").val();
     var name = $("input#userName").val();
     var phoneNumber = $("input#phoneNumber").val();
-
-    $(".address").text(address);
-    $(".userName").text(name);
-    $(".phoneNumber").text(phoneNumber);
+    $("ul.contacts").append("<li class='contact'><h2 class ='userName'>"+name+"</h2><p class='information'><span class ='address'>"+address+"</span><br><span class ='phoneNumber'>"+phoneNumber+"</span></p></li>")
     $(".output").show();
     event.preventDefault();
+
+    $("li.contact").last().click(function() {
+      $(this).children(".userName").toggle();
+      $(this).children(".information").toggle();
+      // $(this).children("ul#userName").after(".output");
+    });
+
   });
 
-
-  $(".output").click(function() {
-    $(".userName").toggle();
-    $(".information").toggle();
-  });
 });
